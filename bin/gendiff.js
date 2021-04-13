a06fbfc8d7743e76d54fb876abb0db16a230e6c3
Bogdan Pugachuk <intheend54@gmail.com>
2021-04-13 12:24:33 +0500

    Add format option and configure arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,9 +3,11 @@
 import { Command } from '../node_modules/commander/esm.mjs';
 
 const program = new Command();
-program.version('0.0.1');
 
 program
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
