860e4551a3390b5130e4ea400e7d09393863d0cb
Bogdan Pugachuk <intheend54@gmail.com>
2021-09-13 15:00:48 +0500

    Edit all fixtures files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,17 +10,61 @@ const filepath4 = getFixturePath('file2.yml');
 const filepath5 = getFixturePath('file1.yaml');
 const filepath6 = getFixturePath('file2.yaml');
 
-const expected = `{
- - follow: false
-   host: hexlet.io
- - proxy: 123.234.53.22
- - timeout: 50
- + timeout: 20
- + verbose: true
-}`;
+const expected = `
+{
+  common: {
+    + follow: false
+      setting1: Value 1
+    - setting2: 200
+    - setting3: true
+    + setting3: null
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+      setting6: {
+          doge: {
+            - wow: 
+            + wow: so much
+          }
+          key: value
+        + ops: vops
+      }
+  }
+  group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+          key: value
+      }
+    + nest: str
+  }
+- group2: {
+      abc: 12345
+      deep: {
+          id: 45
+      }
+  }
++ group3: {
+      deep: {
+          id: {
+              number: 45
+          }
+      }
+      fee: 100500
+  }
+}
+`;
 
-test('gendiff', () => {
+test('gendiff json', () => {
   expect(gendiff(filepath1, filepath2)).toBe(expected);
+});
+
+test('gendiff yml', () => {
   expect(gendiff(filepath3, filepath4)).toBe(expected);
+});
+
+test('gendiff yaml', () => {
   expect(gendiff(filepath5, filepath6)).toBe(expected);
 });
